79d95495a45de480f5f58e535cbcdb6d8a216350
JA70x7 <abreujosias0@gmail.com>
2025-03-28 10:08:14 -0300

    operadores aritmeticos

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,36 @@
+# Adiçao
+numero1 = 10
+numero2 = 20
+
+soma = numero1 + numero2
+print(soma)
+
+# Subtraçao
+
+subtraçao = numero1 - numero2
+print(subtraçao)
+
+# Multiplicaçao
+
+multiplicaçao = numero1 * numero2
+print(multiplicaçao)
+
+# Divisao
+
+divisao = numero1 / numero2
+print(divisao)
+
+# Potencia
+
+potencia = numero1 ** numero2
+print(potencia)
+
+# Modulo
+
+modulo = numero1 % numero2
+print(modulo)
+
+# Resto da divisao
+
+divisao = numero1 // numero2
+print(divisao)
