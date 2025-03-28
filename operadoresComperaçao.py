318daeef6d8a5f70c80e5c75fdf5197db7fab847
JA70x7 <abreujosias0@gmail.com>
2025-03-28 10:47:06 -0300

    operadoresComparaçao

---
+++ operadoresComperaçao.py
@@ -0,0 +1,23 @@
+
+x = 10
+y = 10
+
+# Igualdade
+print(x == y)
+
+# Diferença 
+print(x != y)
+
+# Maior
+print(x > y)
+
+# Menor
+print(x < y)
+
+# Maior Igual
+print(x >= y)
+
+# Menor Igual
+print(x <= y)
+
+
